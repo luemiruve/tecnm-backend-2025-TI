a65e414eb22112f8cc6a247766e582a5ba936f9b
Luis Ruiz Vega <luisruizvega@Luiss-MacBook-Air.local>
2025-11-06 07:57:53 -0600

    creacion de arreglo de productos

--- src/main/java/mx/tecnm/backend/api/api/controllers/Test.java
+++ src/main/java/mx/tecnm/backend/api/api/controllers/Test.java
@@ -23,4 +23,28 @@ public class Test {
 
         return producto;
     }
+
+    @GetMapping("/productos")
+    public Producto[] getProductos(){
+        Producto producto1 = new Producto();
+        producto1.nombreProducto = "Yogurt Danone Fresa";
+        producto1.codigoBarras = "7588443100043";
+        producto1.precio = 12.0;
+
+
+        Producto producto2 = new Producto();
+        producto2.nombreProducto = "Leche Lala 1lt";
+        producto2.codigoBarras = "7588443190343";
+        producto2.precio = 22.50;
+
+        Producto producto3 = new Producto();
+        producto3.nombreProducto = "Queso manchego 200g";
+        producto3.codigoBarras = "7505643190343";
+        producto3.precio = 45.75;
+
+        Producto productos[] = {producto1, producto2, producto3};
+
+
+        return productos;
+    }
 }
