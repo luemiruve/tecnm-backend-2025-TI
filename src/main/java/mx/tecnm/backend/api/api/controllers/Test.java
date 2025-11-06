7b8952d819b26c4ebff78cdaf11ebb359acbc58b
Luis Ruiz Vega <luisruizvega@Luiss-MacBook-Air.local>
2025-11-06 07:41:21 -0600

    creacion de clase producto

--- src/main/java/mx/tecnm/backend/api/api/controllers/Test.java
+++ src/main/java/mx/tecnm/backend/api/api/controllers/Test.java
@@ -1,5 +1,6 @@
 package mx.tecnm.backend.api.api.controllers;
 
+import mx.tecnm.backend.api.api.modells.Producto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,15 @@ public class Test {
     @GetMapping("/hello")
     public String helloWorld(){
         return "hello word";
+    }
+
+    @GetMapping("/producto")
+    public Producto getProducto(){
+        Producto producto = new Producto();
+        producto.nombreProducto = "Yogurt Danone Fresa";
+        producto.codigoBarras = "7588443100043";
+        producto.precio = 12.0;
+
+        return producto;
     }
 }
---
+++ src/main/java/mx/tecnm/backend/api/api/modells/Producto.java
@@ -0,0 +1,7 @@
+package mx.tecnm.backend.api.api.modells;
+
+public class Producto {
+   public String nombreProducto;
+    public String codigoBarras;
+    public Double precio;
+}
